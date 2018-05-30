ab89206c14476acc5bf44e59b8df1815ca83f355
Ben Ayers-Glassey <bayersglassey@gmail.com>
2018-05-29 23:24:02 -0700

    src/main_sdl.c: keep source code within 80 cols...

--- c/src/main_sdl.c
+++ c/src/main_sdl.c
@@ -9,7 +9,10 @@
 
 
 #define ERR_INFO() fprintf(stderr, "%s: %i: ", __func__, __LINE__)
-#define RET_IF_SDL_ERR(x) {int e=(x); if(e < 0){ERR_INFO(); fprintf(stderr, "SDL error: %s\n", SDL_GetError()); return 2;}}
+#define RET_IF_SDL_ERR(x) {int e=(x); \
+    if(e < 0){ERR_INFO(); \
+    fprintf(stderr, "SDL error: %s\n", SDL_GetError()); \
+    return 2;}}
 
 
 
@@ -26,30 +29,44 @@ int mainloop(SDL_Renderer *renderer, int n_args, char *args[]){
     while(loop){
         if(refresh){
             refresh = false;
-            RET_IF_SDL_ERR(SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255));
+            RET_IF_SDL_ERR(SDL_SetRenderDrawColor(renderer,
+                0, 0, 0, 255));
             RET_IF_SDL_ERR(SDL_RenderClear(renderer));
             /* render something... */
             SDL_RenderPresent(renderer);
         }
-        SDL_PollEvent(&event);
-        switch(event.type){
-            case SDL_KEYDOWN:
-                if(event.key.keysym.sym == SDLK_ESCAPE)loop = false;
-                if(event.key.keysym.sym == SDLK_0){rot = 0; refresh = true;}
-                if(event.key.keysym.sym == SDLK_LEFT && keydown_l == 0)keydown_l = 2;
-                if(event.key.keysym.sym == SDLK_RIGHT && keydown_r == 0)keydown_r = 2;
-                if(event.key.keysym.sym == SDLK_LSHIFT || event.key.keysym.sym == SDLK_RSHIFT)keydown_shift = true;
-                break;
-            case SDL_KEYUP:
-                if(event.key.keysym.sym == SDLK_LEFT)keydown_l = 0;
-                if(event.key.keysym.sym == SDLK_RIGHT)keydown_r = 0;
-                if(event.key.keysym.sym == SDLK_LSHIFT || event.key.keysym.sym == SDLK_RSHIFT)keydown_shift = false;
-                break;
-            case SDL_QUIT: loop = false; break;
-            default: break;
+        while(SDL_PollEvent(&event)){
+            switch(event.type){
+                case SDL_KEYDOWN:
+                    if(event.key.keysym.sym == SDLK_ESCAPE){
+                        loop = false;}
+                    if(event.key.keysym.sym == SDLK_0){
+                        rot = 0; refresh = true;}
+                    if(event.key.keysym.sym == SDLK_LEFT && keydown_l == 0){
+                        keydown_l = 2;}
+                    if(event.key.keysym.sym == SDLK_RIGHT && keydown_r == 0){
+                        keydown_r = 2;}
+                    if(event.key.keysym.sym == SDLK_LSHIFT
+                        || event.key.keysym.sym == SDLK_RSHIFT){
+                            keydown_shift = true;}
+                    break;
+                case SDL_KEYUP:
+                    if(event.key.keysym.sym == SDLK_LEFT){
+                        keydown_l = 0;}
+                    if(event.key.keysym.sym == SDLK_RIGHT){
+                        keydown_r = 0;}
+                    if(event.key.keysym.sym == SDLK_LSHIFT
+                        || event.key.keysym.sym == SDLK_RSHIFT){
+                            keydown_shift = false;}
+                    break;
+                case SDL_QUIT: loop = false; break;
+                default: break;
+            }
         }
-        if(keydown_l >= (keydown_shift? 2: 1)){rot += 1; refresh = true; keydown_l = 1;}
-        if(keydown_r >= (keydown_shift? 2: 1)){rot -= 1; refresh = true; keydown_r = 1;}
+        if(keydown_l >= (keydown_shift? 2: 1)){
+            rot += 1; refresh = true; keydown_l = 1;}
+        if(keydown_r >= (keydown_shift? 2: 1)){
+            rot -= 1; refresh = true; keydown_r = 1;}
     }
     return 0;
 }
@@ -66,14 +83,16 @@ int main(int n_args, char *args[]){
 
         if(!window){
             e = 1;
-            fprintf(stderr, "SDL_CreateWindow error: %s\n", SDL_GetError());
+            fprintf(stderr, "SDL_CreateWindow error: %s\n",
+                SDL_GetError());
         }else{
             SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
                 SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
             if(!renderer){
                 e = 1;
-                fprintf(stderr, "SDL_CreateRenderer error: %s\n", SDL_GetError());
+                fprintf(stderr, "SDL_CreateRenderer error: %s\n",
+                    SDL_GetError());
             }else{
                 e = mainloop(renderer, n_args, args);
                 SDL_DestroyRenderer(renderer);
