19c0add6ac692e9b25536367ca0a3df576ed9dad
Ben Ayers-Glassey <bayersglassey@gmail.com>
2018-06-04 09:15:46 -0700

    added src/llist.h

---
+++ c/src/llist.h
@@ -0,0 +1,27 @@
+
+#define LLIST_DECL(T, list) \
+    T *list; \
+    T **list##_last;
+
+#define LLIST_INIT(parent, list) \
+    (parent).list = NULL; \
+    (parent).list##_last = &(parent).list;
+
+#define LLIST_PUSH(T, parent, list, new_node) \
+    T *new_node = calloc(1, sizeof(*new_node)); \
+    if(new_node == NULL)return 1; \
+    *(parent).list##_last = new_node; \
+    (parent).list##_last = &new_node->next;
+
+#define LLIST_FREE(T, parent, list, node_cleanup) { \
+    T *node = (parent).list; \
+    while(node != NULL){ \
+        T *next = node->next; \
+        node_cleanup(node); \
+        free(node); \
+        node = next; \
+    } \
+    (parent).list = NULL; \
+    (parent).list##_last = &(parent).list; \
+}
+
