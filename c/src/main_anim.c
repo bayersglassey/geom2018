ad21032db9452b98cd86dde93ab434c7bb86dfb3
Ben Ayers-Glassey <bayersglassey@gmail.com>
2018-06-13 01:25:09 -0700

    main_anim.c: load a hexcollmapset, as well as anims

--- c/src/main_anim.c
+++ c/src/main_anim.c
@@ -10,12 +10,20 @@ int mainloop(){
     int err;
 
     stateset_t stateset;
-    const char *filename = "data/anim.fus";
-    err = stateset_load(&stateset, filename);
+    const char *stateset_filename = "data/anim.fus";
+    err = stateset_load(&stateset, stateset_filename);
     if(err)return err;
 
     stateset_dump(&stateset, stdout);
     stateset_cleanup(&stateset);
+
+    hexcollmapset_t hexcollmapset;
+    const char *hexcollmapset_filename = "data/map.fus";
+    err = hexcollmapset_load(&hexcollmapset, hexcollmapset_filename);
+    if(err)return err;
+
+    hexcollmapset_dump(&hexcollmapset, stdout);
+    hexcollmapset_cleanup(&hexcollmapset);
 
     return 0;
 }
