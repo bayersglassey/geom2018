b487fb74cebe492cc5aad5bdff29f6d5b85bb2fd
Ben Ayers-Glassey <bayersglassey@gmail.com>
2018-06-17 21:35:17 -0700

    hexspace.[ch]: implemented rot/flip/render, added vec4_{vec,rot}_from_hexspace

--- c/src/hexspace.c
+++ c/src/hexspace.c
@@ -9,6 +9,16 @@
 #define HEXSPACE_ROT_MAX 6
 
 
+/*
+
+  Y +
+     \
+     (+)- +
+          X
+
+*/
+
+
 vecspace_t hexspace = {
     HEXSPACE_DIMS,
     HEXSPACE_ROT_MAX,
@@ -24,34 +34,47 @@ void hexspace_set(vec_t v, int x, int y){
 }
 
 void hexspace_rot(vec_t v, rot_t r){
-    fprintf(stderr, "TODO: IMPLEMENT HEXSPACE ROT\n");
-
     /* Let's be saaafe */
     r = rot_contain(HEXSPACE_ROT_MAX, r);
 
     for(int i = r; i > 0; i--){
-        int x = v[0];
-        int y = v[1];
+        /* RX = X + Y */
+        /* RY = -X */
+        int x = v[0] - v[1];
+        int y = v[0];
         v[0] = -x;
         v[1] = y;
     }
 }
 
 void hexspace_flip(vec_t v, flip_t flip){
-    fprintf(stderr, "TODO: IMPLEMENT HEXSPACE FLIP\n");
-
     if(flip){
-        int x = v[0];
-        int y = v[1];
+        /* RX = X */
+        /* RY = -X - Y */
+        int x = v[0] - v[1];
+        int y = -v[1];
         v[0] = x;
         v[1] = -y;
     }
 }
 
 void hexspace_render(vec_t v, int *x, int *y){
-    fprintf(stderr, "TODO: IMPLEMENT HEXSPACE RENDER\n");
+    /* Basically we transform hexspace to vec4, then render that.
+    Hexspace to vec4: X -> A, Y -> C - A */
 
-    *x = v[0];
-    *y = -v[1];
+    *x = 2 * v[0] - v[1];
+    *y = -2 * v[1];
+}
+
+void vec4_vec_from_hexspace(vec_t v, vec_t w){
+    /* Hexspace to vec4: X -> A, Y -> C - A */
+    v[0] = w[0] - w[1];
+    v[1] = 0;
+    v[2] = w[1];
+    v[3] = 0;
+}
+
+rot_t vec4_rot_from_hexspace(rot_t r){
+    return r * 2;
 }
 
--- c/src/hexspace.h
+++ c/src/hexspace.h
@@ -9,6 +9,10 @@ void hexspace_rot(vec_t v, rot_t r);
 void hexspace_flip(vec_t v, flip_t flip);
 void hexspace_render(vec_t v, int *x, int *y);
 
+void vec4_vec_from_hexspace(vec_t v, vec_t w);
+rot_t vec4_rot_from_hexspace(rot_t r);
+
+
 extern vecspace_t hexspace;
 
 
