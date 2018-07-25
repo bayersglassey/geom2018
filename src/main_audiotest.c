1cb4e965c67a51b22b4598a7264f5a74681b275f
Ben Ayers-Glassey <bayersglassey@gmail.com>
2018-07-25 01:06:42 -0700

    main_audiotest.c: factored audiotest() out of main(), added simple audio REPL (as it were)

--- src/main_audiotest.c
+++ src/main_audiotest.c
@@ -10,6 +10,61 @@
 #include "audio.h"
 
 
+int audiotest(SDL_AudioDeviceID dev, const char *filename){
+    int err;
+
+    audio_buffer_t buf;
+    audio_parser_t parser;
+    err = audio_parser_init(&parser, &buf, 250);
+    if(err)return err;
+
+    printf("Loading %s...\n", filename);
+    char *text = load_file(filename);
+    if(text == NULL)return 1;
+    {
+        fus_lexer_t lexer;
+        err = fus_lexer_init(&lexer, text, filename);
+        if(err)return err;
+        err = audio_parser_parse(&parser, &lexer);
+        if(err)return err;
+        fus_lexer_cleanup(&lexer);
+    }
+
+    printf("Playing!.. enter more music, or \"@exit\" to quit.\n");
+    printf("WARNING: don't define new variables with \"@def\", because "
+        "the text buffer is overwritten every time you hit enter...\n");
+    SDL_PauseAudioDevice(dev, 0);
+
+    int pos = 0;
+    while(1){
+        int end = int_min(buf.data_len, parser.pos);
+        int len = end - pos;
+        printf("Parser at sample %i/%i\n", parser.pos, buf.data_len);
+        printf("Queueing %i samples starting at %i...\n", len, pos);
+        RET_IF_SDL_NZ(SDL_QueueAudio(dev, buf.data + pos,
+            len * sizeof(audio_sample_t)));
+        pos += len;
+
+        printf("> ");
+        char buffer[200];
+        err = getln(buffer, 200);
+        if(err)return err;
+        if(!strcmp(buffer, "@exit"))break;
+
+        fus_lexer_t lexer;
+        err = fus_lexer_init(&lexer, buffer, "<user input>");
+        if(err)return err;
+        err = audio_parser_parse(&parser, &lexer);
+        if(err)return err;
+        fus_lexer_cleanup(&lexer);
+    }
+
+    audio_parser_cleanup(&parser);
+    free(text);
+    return 0;
+}
+
+
 int main(int n_args, char *args[]){
     int err;
 
@@ -34,19 +89,8 @@ int main(int n_args, char *args[]){
     }
 
     const char *filename = "data/music/test.fus";
-    printf("Loading %s...\n", filename);
-    audio_buffer_t buf;
-    err = audio_buffer_load(&buf, filename);
+    err = audiotest(dev, filename);
     if(err)return err;
-
-    printf("Queueing %i samples...\n", buf.data_len);
-    RET_IF_SDL_NZ(SDL_QueueAudio(dev, buf.data,
-        buf.data_len * sizeof(audio_sample_t)));
-
-    SDL_PauseAudioDevice(dev, 0);
-
-    printf("Playing!.. press Enter to quit.\n");
-    char c = getchar();
 
     SDL_CloseAudioDevice(dev);
 
