a3f8f6eebd13f89d8d5f6b09639740f0382f38e8
Ben Ayers-Glassey <bayersglassey@gmail.com>
2020-12-12 00:06:21 -0800

    deploy.sh now does: git reset --hard origin/master; git status

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,2 +1,3 @@
 . ip.sh
-ssh depths@$IP 'cd /srv/depths && git pull'
+ssh depths@$IP 'cd /srv/depths && git fetch && git reset --hard origin/master; git status'
+#rsync -v -a --delete -e ssh ./www/ depths@$IP:/srv/depths/www/
