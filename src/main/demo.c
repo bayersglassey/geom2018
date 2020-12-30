b74a4579fde4d2f7af0fda2847864280f6dd8863
Ben Ayers-Glassey <bayersglassey@gmail.com>
2020-12-29 21:15:23 -0800

    main/demo.c: added print_help

--- src/main/demo.c
+++ src/main/demo.c
@@ -15,6 +15,12 @@
 #define DELAY_GOAL 30
 
 
+const char* DEFAULT_PREND_FILENAME = "data/test.fus";
+const char* DEFAULT_STATESET_FILENAME = "anim/player.fus";
+const int DEFAULT_PLAYERS = 2;
+const int DEFAULT_PLAYERS_PLAYING = 1;
+
+
 #ifdef __EMSCRIPTEN__
 #include "../emscripten.h"
 static void test_app_mainloop_emcc(void *arg){
@@ -28,24 +34,48 @@ static void test_app_mainloop_emcc(void *arg){
 #endif
 
 
+void print_help(FILE *file){
+    fprintf(file,
+        "Options:\n"
+        "   -h   --help           Shows this message\n"
+        "   -F                    Fullscreen mode\n"
+        "   -FD                   Software simulated fullscreen mode\n"
+        "   -f            FILE    Prismelrenderer filename (default: %s)\n"
+        "   -a   --anim   FILE    Stateset filename (default: %s)\n"
+        "   -m   --map    FILE    Map filename (default: NULL)\n"
+        "   -sm  --submap FILE    Submap filename (default: NULL)\n"
+        "   -d   --devel          Developer mode\n"
+        "   -p   --players   N    Number of players (default: %i)\n"
+        "        --minimap_alt    Use alternate minimap\n"
+        "        --dont_cache_bitmaps\n"
+        "                         ...low-level hokey-pokery, should probably get rid of this option\n"
+        , DEFAULT_PREND_FILENAME, DEFAULT_STATESET_FILENAME, DEFAULT_PLAYERS_PLAYING
+    );
+}
+
+
 int main(int n_args, char *args[]){
     int e = 0;
     Uint32 window_flags = SDL_WINDOW_SHOWN;
-    const char *prend_filename = "data/test.fus";
-    const char *stateset_filename = "anim/player.fus";
+    const char *prend_filename = DEFAULT_PREND_FILENAME;
+    const char *stateset_filename = DEFAULT_STATESET_FILENAME;
     const char *hexmap_filename = NULL;
     const char *submap_filename = NULL;
     bool minimap_alt = true;
     bool cache_bitmaps = true;
-    int n_players = 2;
-    int n_players_playing = 1;
+    bool developer_mode = false;
+    int n_players = DEFAULT_PLAYERS;
+    int n_players_playing = DEFAULT_PLAYERS_PLAYING;
 
     /* The classic */
     srand(time(0));
 
     for(int arg_i = 1; arg_i < n_args; arg_i++){
         char *arg = args[arg_i];
-        if(!strcmp(arg, "-F")){
+        if(!strcmp(arg, "-h") || !strcmp(arg, "--help")){
+            print_help(stdout);
+            return 0;
+        }else if(!strcmp(arg, "-F")){
             window_flags |= SDL_WINDOW_FULLSCREEN;
         }else if(!strcmp(arg, "-FD")){
             window_flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
@@ -53,47 +83,66 @@ int main(int n_args, char *args[]){
             arg_i++;
             if(arg_i >= n_args){
                 fprintf(stderr, "Missing filename after %s\n", arg);
-                return 2;}
+                goto parse_failure;
+            }
             arg = args[arg_i];
             prend_filename = arg;
-        }else if(!strcmp(arg, "--anim")){
+        }else if(!strcmp(arg, "-a") || !strcmp(arg, "--anim")){
             arg_i++;
             if(arg_i >= n_args){
                 fprintf(stderr, "Missing filename after %s\n", arg);
-                return 2;}
+                goto parse_failure;
+            }
             arg = args[arg_i];
             stateset_filename = arg;
-        }else if(!strcmp(arg, "--map")){
+        }else if(!strcmp(arg, "-m") || !strcmp(arg, "--map")){
             arg_i++;
             if(arg_i >= n_args){
                 fprintf(stderr, "Missing filename after %s\n", arg);
-                return 2;}
+                goto parse_failure;
+            }
             arg = args[arg_i];
             hexmap_filename = arg;
-        }else if(!strcmp(arg, "--submap")){
+        }else if(!strcmp(arg, "-sm") || !strcmp(arg, "--submap")){
             arg_i++;
             if(arg_i >= n_args){
                 fprintf(stderr, "Missing filename after %s\n", arg);
-                return 2;}
+                goto parse_failure;
+            }
             arg = args[arg_i];
             submap_filename = arg;
+        }else if(!strcmp(arg, "-d") || !strcmp(arg, "--devel")){
+            developer_mode = true;
+        }else if(!strcmp(arg, "-p") || !strcmp(arg, "--players")){
+            arg_i++;
+            if(arg_i >= n_args){
+                fprintf(stderr, "Missing int after %s\n", arg);
+                goto parse_failure;
+            }
+            arg = args[arg_i];
+            n_players = atoi(arg);
+            n_players_playing = n_players;
+            if(n_players_playing <= 0){
+                fprintf(stderr,
+                    "Number of players must be greater than 0. Got: %i\n",
+                    n_players_playing);
+                goto parse_failure;
+            }
+            fprintf(stderr, "Number of players set to %i\n", n_players);
         }else if(!strcmp(arg, "--minimap_alt")){
             minimap_alt = !minimap_alt;
         }else if(!strcmp(arg, "--dont_cache_bitmaps")){
             cache_bitmaps = false;
-        }else if(!strcmp(arg, "--players")){
-            arg_i++;
-            if(arg_i >= n_args){
-                fprintf(stderr, "Missing int after %s\n", arg);
-                return 2;}
-            arg = args[arg_i];
-            n_players = atoi(arg);
-            n_players_playing = n_players;
-            fprintf(stderr, "Number of players set to %i\n", n_players);
         }else{
             fprintf(stderr, "Unrecognized option: %s\n", arg);
-            return 2;
+            goto parse_failure;
         }
+
+        continue;
+        parse_failure:
+        fputc('\n', stderr);
+        print_help(stderr);
+        return 2;
     }
 
     if(SDL_Init(SDL_INIT_VIDEO)){
