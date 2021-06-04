71b8f1ebb606dce41c1b3cf36f41b8170dbf7633
Ben Ayers-Glassey <bayersglassey@gmail.com>
2021-06-03 21:02:30 -0700

    Fixed compile_www.sh, deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,5 @@
+set -euo pipefail
 . ip.sh
 ssh depths@$IP 'cd /srv/depths && git fetch && git reset --hard origin/master; git status'
 rsync -v -a --delete -e ssh ./www/ depths@$IP:/srv/depths/www/
+ssh depths@$IP 'cd /srv/depths && ln -s $PWD www/repo'
