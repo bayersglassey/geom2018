171e0907c7076f896d71a07c28384e9966c13b29
Ben Ayers-Glassey <bayersglassey@gmail.com>
2021-08-31 23:07:15 -0700

    deploy.sh: mention the fact that we're adding a symlink for the repo

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,4 +2,4 @@ set -euo pipefail
 . ip.sh
 ssh depths@$IP 'cd /srv/depths && git fetch && git reset --hard origin/master; git status'
 rsync -v -a --delete -e ssh ./www/ depths@$IP:/srv/depths/www/
-ssh depths@$IP 'cd /srv/depths && ln -s $PWD www/repo'
+ssh depths@$IP 'cd /srv/depths && echo "linking repo" && ln -s $PWD www/repo'
