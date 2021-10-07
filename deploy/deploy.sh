2d3692a3652fbecc7083aec0ec9cc5037f529228
Ben Ayers-Glassey <bayersglassey@gmail.com>
2021-10-06 22:07:19 -0700

    updated deploy.sh (just adding some handy output messages)

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,13 @@
 set -euo pipefail
 . ip.sh
+
+echo "### Updating repo..." >&2
 ssh depths@$IP 'cd /srv/depths && git fetch && git reset --hard origin/master; git status'
+
+echo "" >&2
+echo "### Updating web assets..." >&2
 rsync -v -a --delete -e ssh ./www/ depths@$IP:/srv/depths/www/
-ssh depths@$IP 'cd /srv/depths && echo "linking repo" && ln -s $PWD www/repo'
+
+echo "" >&2
+echo "### Adding symlink to repo in web assets..." >&2
+ssh depths@$IP 'cd /srv/depths && ln -s $PWD www/repo'
