57254a0c08bd8b828c218aa421bb431ff6a234ef
Ben Ayers-Glassey <bayersglassey@gmail.com>
2021-12-18 23:54:32 -0800

    rec-vlc.sh: added 2 second delay before starting recording

--- rec-vlc.sh
+++ rec-vlc.sh
@@ -2,6 +2,10 @@ set -e
 
 dst="out.mp4"
 
+delay=2
+echo "Waiting $delay seconds before starting the recording..." >&2
+sleep "$delay"
+
 # "https://wiki.videolan.org/Codec/" sayeth:
 # "For portability to almost all decoders, we recommend using the MPEG-1 standard of vcodec=mp1v, acodec=mpga, and mux=mpeg1"
 # Should we perhaps change our vcodec from h264 to mp1v?..
