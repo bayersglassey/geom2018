f154822050cd548d482ab56a95acd69fb9f800e8
Ben Ayers-Glassey <bayersglassey@gmail.com>
2021-12-20 23:07:40 -0800

    rename_thing.sh: caller can pass a list of dirs to search in

--- rename_thing.sh
+++ rename_thing.sh
@@ -9,22 +9,23 @@ test "$#" -ge 2 || {
 
 from="$1"
 to="$2"
+shift 2
 
 
-echo "Renaming: $from -> $to" >&2
+echo "### Renaming: $from -> $to" >&2
 
 
-if ack -l "$from" >/dev/null
+if ack -l "$from" "$@" >/dev/null
 then
     echo "Modifying the following files:" >&2
-    ack -l "$from" >&2
-    sed -i "s@$from@$to@g" $(ack -l "$from") 2>/dev/null || true
+    ack -l "$from" "$@" >&2
+    sed -i "s@$from@$to@g" $(ack -l "$from" "$@") || true
 else
     echo "No files need to be modified" >&2
 fi
 
 found=
-for f in $(find -name "*$from*.fus")
+for f in $(find "$@" -name "*$from*")
 do
     found=1
     f2="$(echo "$f" | sed "s@$from@$to@g")"
