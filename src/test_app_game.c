727b793756bc012332f517bba0ecbadcb9c04d13
Ben Ayers-Glassey <bayersglassey@gmail.com>
2022-01-05 00:29:18 -0800

    Hitting tab now shows minimap with zoom=2 first, then zoom=1

--- src/test_app_game.c
+++ src/test_app_game.c
@@ -206,7 +206,7 @@ int test_app_process_event_game(test_app_t *app, SDL_Event *event){
         }else if(event->key.keysym.sym == SDLK_TAB){
             /* Cycle between 3 values: 0 means don't show minimap, and
             1 and 2 are zoom values. */
-            game->show_minimap = (game->show_minimap + 1) % 3;
+            game->show_minimap = (game->show_minimap + 3 - 1) % 3;
         }else if(event->key.keysym.sym == SDLK_F6 && app->developer_mode){
             /* Hack, we really want to force camera->mapper to NULL, but
             instead we assume the existence of this mapper called "single" */
