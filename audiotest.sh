cc3261bd7e960092c178475416608f9e29c06931
Ben Ayers-Glassey <bayersglassey@gmail.com>
2024-01-03 16:27:11 -0800

    added audiotest.sh, removed automake.sh

---
+++ audiotest.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -euo pipefail
+
+mkdir -p output
+cfile=output/music.c
+binfile=output/music
+
+echo "
+#include \"stdio.h\"
+#include \"stdlib.h\"
+typedef unsigned int I;
+int getint(const char *name, int default_value){
+    const char *value = getenv(name);
+    return value? atoi(value): default_value;
+}
+int main(){
+    int quiet = getenv(\"QUIET\")? 1: 0;
+    int skip = getint(\"SKIP\", 32);
+    for(I t=0;;t++){
+        I a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,u,v,w,x,y,z;
+        unsigned char _c = (c=0, ($1));
+        unsigned char _c2 = (c=1, ($1));
+        putchar(_c);
+        putchar(_c2);
+        if (quiet) continue;
+        if (t % skip) continue;
+        fprintf(stderr, \"%3i \", _c);
+        for (int i = 0; i < _c/2; i++) fputs(\"█\", stderr);
+        if (_c%2) fputs(\"▌\", stderr);
+        fputc('\n', stderr);
+    }
+    return 0;
+}" >"$cfile" &&
+gcc "$cfile" -o "$binfile" &&
+./"$binfile" | aplay -c "${CHANNELS:-2}" -f "${FORMAT:-U8}" -r "${RATE:-8000}"
--- automake.sh
+++
@@ -1,5 +0,0 @@
-# WARNING: THIS IS UNUSED.
-# It's a ganky thing which probably won't actually work.
-# But it took too long to figure out how to achieve it (unix tools at
-# their worst), so I'm gonna hold onto it in source control.
-gcc -MM src/*.c | sed -E -e 's|^(.*.o:)|src/\1|'
